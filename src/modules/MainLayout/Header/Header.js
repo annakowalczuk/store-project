9e710f388115b08919719291823158a48928a89e
Anna Kowalczuk <anna@kowalczuk.eu>
2019-01-18 21:05:12 +0100

    Add RWD for topbar, fix styles

--- src/modules/MainLayout/Header/Header.js
+++ src/modules/MainLayout/Header/Header.js
@@ -13,11 +13,11 @@ export const Header = props => {
     <div className='section--topnav'>
       <Container>
         <Row>
-          <Col md='12' lg='4'>
+          <Col md='12' lg='4' className='topnav-logo' >
             <Logo />
           </Col>
-          <Col md='12' lg='7'>
-            <nav className='topnav-nav'>
+          <Col md='12' lg='7' className='topnav-nav' >
+            <nav>
               <NavLink exact to='/' className= 'topnav-link' id='topnav-home' activeClassName='active'>Home</NavLink>
               <NavLink exact to='/faq' className= 'topnav-link' activeClassName='active'>FAQ</NavLink>
               <NavLink exact to='/terms' className= 'topnav-link' activeClassName='active'>Regulamin</NavLink>
