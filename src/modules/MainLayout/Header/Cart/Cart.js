b2770aaa144d71a9ec37f2288223051153900e0b
Anna Kowalczuk <anna@kowalczuk.eu>
2019-01-26 12:30:28 +0100

    Add onMouseEnter and onMouseLeave event hendlers for Products

--- src/modules/MainLayout/Header/Cart/Cart.js
+++ src/modules/MainLayout/Header/Cart/Cart.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import CartIcon from './cart-icon.png';
-
 import './Cart.scss';
 
 const Cart = (props) => {
   return (
     <div className='cart-icon-container'>
-      <img src={CartIcon} className='cart-icon' alt='cart icon' />
+      <i className="fas fa-shopping-cart"></i>
     </div>
     );
 };
--- src/modules/Product/Product.js
+++ src/modules/Product/Product.js
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Product.scss';
 
-const Product = (props) => {
-
-  const { product } = props;
-  const imgUrl = require(`../../data/images/${product.index}.png`);
-
-  const renderAdditionalInfo = () => {
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovering: false,
+    };
+  }
+  
+  renderAdditionalInfo = () => {
     return (
-      product.additional_info
+      this.props.product.additional_info
     );
   }
-  return (
-    <div className='product-box' >
-      <div className='top-info'>
-      {product.additional_info ? renderAdditionalInfo() : null }
-      </div>
-      <div className='photo-product' style={{ backgroundImage: `url(${imgUrl})` }} >
-      </div>
-      <div className='bottom-info'>
-        <div>
-          <div className='product-name' >{product.name}</div>
-          <div className='product-price' >${product.price}</div>
+  
+  handleMouseHover = () => {
+    this.setState(this.toggleHoverState);
+  }
+
+  toggleHoverState(state) {
+    return {
+      isHovering: !state.isHovering,
+    };
+  }
+
+  render() {
+    const { product } = this.props;
+    // url for images rendering
+    const imgUrl = `/images/${product.index}.jpg`;
+
+    return (
+      <div>
+        <div className={(this.state.isHovering === true) ? 'product-box active' : 'product-box'}
+          onMouseEnter={this.handleMouseHover}
+          onMouseLeave={this.handleMouseHover}>
+          <div className='top-info'>
+            <div>
+              {product.additional_info ? this.renderAdditionalInfo() : null }
+            </div>
+            <div>
+              <i className="far fa-heart"></i>
+            </div>
+          </div>
+          <div className='photo-product' style={{ backgroundImage: `url(${imgUrl})` }} >
+          </div>
+          <div className='bottom-info'>
+            <div>
+              <div className='product-name' >{product.name}</div>
+              <div className='product-price' >${product.price}</div>
+            </div>
+            <div className='addToCart'>
+              <span>Add to cart</span>
+              <i className="fas fa-cart-plus"></i>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Product;
