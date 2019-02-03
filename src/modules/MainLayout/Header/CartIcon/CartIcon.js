eb27c9b95c6a72d8971783859e14a6766688581b
Anna Kowalczuk <anna@kowalczuk.eu>
2019-02-03 23:02:50 +0100

    Add indicator showing how many items are in the cart

--- src/modules/MainLayout/Header/CartIcon/CartIcon.js
+++ src/modules/MainLayout/Header/CartIcon/CartIcon.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './CartIcon.scss';
 
 const CartIcon = (props) => {
+
+  const renderItemsInCart = () => {
+    return (
+      <div className='itemsInCart' >{props.itemsInCart}</div>
+    );
+  }
+
   return (
     <div className='cart-icon-container'>
     <Link to="/cart"><i className="fas fa-shopping-cart"></i></Link>
+    { props.itemsInCart === 0 ? null : renderItemsInCart() }
     </div>
     );
 };
 
-export default CartIcon;
+const mapStateToProps = (state) => {
+  return {
+    itemsInCart: Object.values(state.cart).reduce((a,b) => a + b, 0),
+  };
+};
+
+export default connect(mapStateToProps)(CartIcon);
