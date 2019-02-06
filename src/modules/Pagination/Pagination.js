f939015f6fb505980abd31f772e1d76de204bb75
Anna Kowalczuk <anna@kowalczuk.eu>
2019-02-07 00:31:30 +0100

    Add partial implementation of pagination

--- src/modules/Pagination/Pagination.js
+++ src/modules/Pagination/Pagination.js
@@ -7,14 +7,53 @@ import './Pagination.scss';
 
 
 const Pagination = (props) => {
-  const productsPerPage = 6;
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(props.products.length / productsPerPage); i++) {
-    pageNumbers.push(i);
-  }
+  const {currentPage} = props;
   
+  const renderPagination = () => {
+    const productsPerPage = 6;
+    const totalPages = Math.ceil(props.products.length / productsPerPage);
+    console.log(totalPages);
+    let paginationItems = [];
+    
+    let firstPaginationItem = Number(currentPage) - 2;
+    let lastPaginationItem = Number(currentPage) + 2;
+    
+    if (Number(currentPage) === 1) {
+      firstPaginationItem = Number(currentPage);
+      lastPaginationItem = Number(currentPage) + 4;
+    }
+    if (Number(currentPage) === 2) {
+      firstPaginationItem = Number(currentPage) - 1;
+      lastPaginationItem = Number(currentPage) + 3;
+    }
+    if (Number(currentPage) === totalPages) {
+      firstPaginationItem = Number(currentPage) - 4;
+      lastPaginationItem = Number(currentPage);
+    }
+    if (Number(currentPage) === totalPages - 1) {
+      firstPaginationItem = Number(currentPage) - 3;
+      lastPaginationItem = Number(currentPage) + 1;
+    }
+
+    for (let i = firstPaginationItem; i <= lastPaginationItem; i++) {
+      paginationItems.push(renderPaginationItem(i));
+    }
+    return paginationItems;
+  }
+
   const handleClick = (e) => {
     props.goToPage(e.target.id);
+  }
+
+  const renderPaginationItem = (number) => {
+    return (
+    <li
+      key={number}
+      className={ Number(currentPage) === number ? 'product-pagination-item active' : 'product-pagination-item' }
+      id={number}
+      onClick={handleClick}
+    >{number}</li>
+    );
   }
 
   return (
@@ -24,15 +63,7 @@ const Pagination = (props) => {
           <Col lg='6'></Col>
           <Col lg='6'>
             <ul>
-              {pageNumbers.map(number =>
-                <li
-                  key={number}
-                  id={number}
-                  onClick={handleClick}
-                >
-                  {number}
-                </li>
-              )}
+              {renderPagination()}
             </ul>
           </Col>
         </Row>
@@ -44,7 +75,7 @@ const Pagination = (props) => {
 const mapStateToProps = (state) => {
   return {
     products: state.products.productsList,
-    currantPage: state.products.currantPage,
+    currentPage: state.products.currentPage,
   };
 };
 
